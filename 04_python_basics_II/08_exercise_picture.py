42960e5fb2dfb79cb2cd6f1983361ea5eab85113
geremygustavo <abelmallcu123@gmail.com>
2021-04-12 21:00:08 -0700

    python basic II part1

---
+++ 04_python_basics_II/08_exercise_picture.py
@@ -0,0 +1,20 @@
+# Exercise !!
+from bokeh.colors.groups import pink
+
+picture = [
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 1, 1, 1, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0]
+]
+# print(picture)
+
+new_list = []
+for image in picture:
+    for pixel in image:
+        if pixel:
+            print('*', end='')
+        else:
+            print(' ', end='')
+    print('') # need a new line after each row
